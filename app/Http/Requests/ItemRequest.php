f717e2bc0727a63d742f57b65832be6a94a2e0d9
Josselin Pennors <josselin.pennors@hotmail.fr>
2018-08-10 23:54:39 +0200

    Resolution bug validation item

--- app/Http/Requests/ItemRequest.php
+++ app/Http/Requests/ItemRequest.php
@@ -26,10 +26,10 @@ class ItemRequest extends FormRequest
         return [
             'name'          =>  'string|between:2,191'.($this->isMethod('put')?'':'|required'),
             'quantity'      =>  'integer|min:0|'.($this->isMethod('put')?'':'|required'),
-            'place'         =>  'exists:itemPlaces|integer|min:0'.($this->isMethod('put')?'':'|required'),
+            'place'         =>  'exists:itemPlaces,id|integer|min:0'.($this->isMethod('put')?'':'|required'),
             'status'        =>  'integer|min:1|max:3'.($this->isMethod('put')?'':'|required'),
             'caution'       =>  'integer|min:0'.($this->isMethod('put')?'':'|required'),
-            'type'          =>  'exists:itemTypes|integer|min:0'.($this->isMethod('put')?'':'|required'),
+            'type'          =>  'exists:itemTypes,id|integer|min:0'.($this->isMethod('put')?'':'|required'),
             'association'   =>  'integer|min:0'.($this->isMethod('put')?'':'|required'),
         ];
     }
