0cef8edaf2aa725d31de1aef5f859e4600aaca5f
Josselin Pennors <josselin.pennors@hotmail.fr>
2018-08-10 23:52:57 +0200

    Modification d'une contrainte d'existence pour clé étrangère

--- app/Http/Requests/ItemRequest.php
+++ app/Http/Requests/ItemRequest.php
@@ -26,10 +26,10 @@ class ItemRequest extends FormRequest
         return [
             'name'          =>  'string|between:2,191'.($this->isMethod('put')?'':'|required'),
             'quantity'      =>  'integer|min:0|'.($this->isMethod('put')?'':'|required'),
-            'place'         => 'exists:itemplaces,id|integer|min:0'.($this->isMethod('put')?'':'|required'),
+            'place'         =>  'exists:itemPlaces|integer|min:0'.($this->isMethod('put')?'':'|required'),
             'status'        =>  'integer|min:1|max:3'.($this->isMethod('put')?'':'|required'),
             'caution'       =>  'integer|min:0'.($this->isMethod('put')?'':'|required'),
-            'type'          =>  'exists:itemtypes,id|integer|min:0'.($this->isMethod('put')?'':'|required'),
+            'type'          =>  'exists:itemTypes|integer|min:0'.($this->isMethod('put')?'':'|required'),
             'association'   =>  'integer|min:0'.($this->isMethod('put')?'':'|required'),
         ];
     }
