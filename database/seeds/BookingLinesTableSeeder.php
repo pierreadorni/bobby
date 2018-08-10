48d0379c2d44cf95edb57e14f3a45788ae696a03
Josselin Pennors <josselin.pennors@etu.utc.fr>
2018-08-10 14:27:40 +0200

    Retrait de 2 seeders pour test en prod

--- database/seeds/BookingLinesTableSeeder.php
+++ database/seeds/BookingLinesTableSeeder.php
@@ -12,36 +12,6 @@ class BookingLinesTableSeeder extends Seeder
      */
     public function run()
     {
-        $booking_lines = [
-        	[
-        		'booking' => 1,
-        		'item'	=>	2,
-        		'quantity'	=>	1,
-                'startDate' => '2018-04-14',
-                'endDate' => '2018-04-14',
-                'status' =>  1,
-	        ],
-	        [
-        		'booking' => 1,
-        		'item'	=>	2,
-        		'quantity'	=>	1,
-                'startDate' => '2018-04-14',
-                'endDate' => '2018-04-14',
-                'status' =>  1,
-	        ],
-	        [
-        		'booking' => 3,
-        		'item'	=>	1,
-        		'quantity'	=>	10,
-                'startDate' => '2018-04-14',
-                'endDate' => '2018-04-14',
-                'status' =>  1,
-	        ],
-        ];
-
-        foreach ($booking_lines as $booking_line => $values){
-            BookingLine::create($values);
-        }
 
     }
 }
--- database/seeds/BookingsTableSeeder.php
+++ database/seeds/BookingsTableSeeder.php
@@ -12,36 +12,6 @@ class BookingsTableSeeder extends Seeder
      */
     public function run()
     {
-        $bookings = [
-        	[
-        		'owner' => 1,
-        		'booker'	=>	2,
-        		'user'	=>	1,
-                'status'    =>  1,
-                'cautionReceived' => false,
-                'caution' =>  100,
-	        ],
-	        [
-                'owner' => 2,
-                'booker'    =>  3,
-                'user'  =>  1,
-                'status'    =>  1,
-                'cautionReceived' => true,
-                'caution' =>  100,
-            ],
-	        [
-                'owner' => 1,
-                'booker'    =>  2,
-                'user'  =>  2,
-                'status'    =>  1,
-                'cautionReceived' => false,
-                'caution' =>  100,
-            ],
-        ];
-
-        foreach ($bookings as $booking => $values){
-            Booking::create($values);
-        }
 
     }
 }
