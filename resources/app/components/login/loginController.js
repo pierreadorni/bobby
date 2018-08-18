bbd65af2536dbedecfa1824da7a32b02b1c318d1
Josselin Pennors <josselin.pennors@hotmail.fr>
2018-08-18 09:38:59 +0200

    test pour prod

--- resources/app/components/login/loginController.js
+++ resources/app/components/login/loginController.js
@@ -14,14 +14,13 @@ app.controller('loginCtrl', function($scope, $location, $rootScope, $routeParams
 	}*/
 	
 	if($routeParams.token){
-		$rootScope.auth.login($routeParams.token)
-		/*.then(function(){
+		$rootScope.auth.login($routeParams.token).then(function(){
 			serviceAjax.get('user').then(function(data){
 			console.log(data)
 			$location.path("/");
-		$location.url($location.path());  // Clear des paramètres
+			$location.url($location.path());  // Clear des paramètres
 		})
-		})*/
+	})
 		// On redirige vers la page main
 		/*serviceAjaxPortail.get("user").then(function(data){
 			console.log(data);
