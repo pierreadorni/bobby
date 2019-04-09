f1fc383b316ac85139d5fa7755faf43d12df8dcd
Unknown <josselin.pennors@hotmail.fr>
2019-04-09 09:48:40 +0200

    Remodelage routes API

--- routes/api.php
+++ routes/api.php
@@ -19,65 +19,119 @@ use App\Booking;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/', function () {
     return view('app');
 });
 
 
 Route::prefix('v1')->group(function () {
-	Route::apiResources([
-			'associations'		=> 'AssociationController',
-			'bookings'	=> 'BookingController',
-			//'users'			=> 'UserController',
-			'bookinglines'			=> 'BookingLineController',
-			'items'			=> 'ItemController',
-			'itemtypes'		=> 'ItemTypeController',
-            'itemplaces'    =>  'ItemPlaceController',
-	]);
 
-    Route::get('user', 'UserController@getUser');
+    /**
+    *   AUTHENTIFICATIOn
+    *
+    */
 
-    //Quand on clique sur une catégorie
-    Route::get('items/categories/{categorie}', 'ItemController@itemFromCategorie');
-
-    Route::get('association/items/{uid}', 'ItemController@itemFromAssociation');
-
-    Route::get('booking/assos/{asso_id}', function($asso_id){
-        return $assos = Association::all()->where('id', '<>', $asso_id);
-    });
-
-    Route::get('booking/items/{asso_id}', function($asso_id){
-        return $items = Item::all()->where('association', $asso_id)->where('status', 1);
-    });
-
-    Route::get('booking/validation/item/{item_id}', function($item_id){
-        $item = Item::find($item_id)->caution;
-        return($item);
-    });
-
-    Route::post('booking/validation/items', 'BookingController@calculCaution');
-
-
-	Route::get('userassos', 'UserController@userAssociations');
-
-    Route::get('bookings/asso/{asso_id}', 'BookingController@indexAssociation');
-
-
-//Route::group(['middleware' => 'cors'], function () {
     Route::get('/login', 'LoginController@login');
 
     Route::get('/code', 'LoginController@authorization_code');
 
-    //Route::get('/token', 'LoginController@get_token');
-
-    Route::post('/send', 'MailController@send');
-//});
+    Route::group(['middleware' => 'PortailAuth'], function () {
 
 
-    //Export 
-    Route::get('export/items', 'ItemController@exportItem');
+        /**
+        *   ASSOCIATIONS
+        *
+        */
+
+        Route::apiResource('associations', 'AssociationController');
+
+
+        /**
+        *   BOOKINGS
+        *
+        */
+
+        Route::apiResource('bookings', 'BookingController');
+
+        Route::get('booking/assos/{asso_id}', function($asso_id){
+            // Portail::hasAssociationAdminPermission($asso_id);
+            return $assos = Association::all()->where('id', '<>', $asso_id);
+        });
+
+        Route::get('booking/items/{asso_id}', function($asso_id){
+            // Portail::isAuthenticated();
+            return $items = Item::all()->where('association', $asso_id)->where('status', 1);
+        });
+
+        Route::get('booking/validation/item/{item_id}', function($item_id){
+            // Portail::isAuthenticated();
+            $item = Item::find($item_id)->caution;
+            return($item);
+        });
+
+        Route::post('booking/validation/items', 'BookingController@calculCaution');
+    
+        Route::get('bookings/asso/{asso_id}', 'BookingController@indexAssociation');
+
+
+        /**
+        *   BOOKINGLINES
+        *
+        */
+
+        Route::apiResource('bookinglines', 'BookingLineController');
+
+
+
+        /**
+        *   ITEM
+        *
+        */
+
+        Route::apiResource('items', 'ItemController');
+
+        Route::get('association/items/{uid}', 'ItemController@itemFromAssociation');
+
+        Route::get('items/categories/{categorie}', 'ItemController@itemFromCategorie');
+
+        Route::get('export/items', 'ItemController@exportItem');
+
+
+        /**
+        *   ITEMPLACE
+        *
+        */
+
+        Route::apiResource('itemplaces', 'ItemPlaceController');
+
+
+        /**
+        *   ITEMTYPE
+        *
+        */
+
+        Route::apiResource('itemtypes', 'ItemTypeController');
+
+
+        /**
+        *   MAIL
+        *
+        */
+
+        Route::post('/send', 'MailController@send');
+
+
+        /**
+        *   USER
+        *
+        */
+
+        Route::get('user', 'UserController@getUser');
+
+        Route::get('userassos', 'UserController@userAssociations');
+
+        Route::get('permissions', 'UserController@getPermissions');      
+
+    });
+
 });
