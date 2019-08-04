d0d007370204bbf3497fd36da7346a5f41874f16
Unknown <josselin.pennors@hotmail.fr>
2019-08-05 01:01:32 +0200

    WIP Booking edition

--- resources/app/components/bookings/editBookingController.js
+++ resources/app/components/bookings/editBookingController.js
@@ -8,84 +8,87 @@
  * Controller of the bobbyApp
  */
 angular.module('bobbyApp')
-  .controller('editBookingCtrl', function ($scope, $routeParams, serviceAjax, $location, $filter, $http) {
+  .controller('editBookingCtrl', function ($scope, $routeParams, serviceAjax, $location, $filter, $rootScope, moment) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
+    if (!$rootScope.canBook() && !$rootScope.isAdmin()) {
+      $location.path('/error/403')
+    }
+
     //Current date
     $scope.currentDate = new Date();
 
     $scope.booking_id = $routeParams.id;
 
-    //Autorisation pour l'utilisateur avant d'avoir chargé les données
-    $scope.memberAssoOwner = false;
-    $scope.memberAssoBooker = false;
-
-    //association de l'utilisateur connecté
-    $scope.assoUser = "1";
+    var loadDates = function(){
+      // Pour les items de la commande, formatage de la date au bon format
+      for (let index = 0; index < $scope.booking.bookinglines.length; index++) {
+        const startDate = $scope.booking.bookinglines[index].startDate;
+        const endDate = $scope.booking.bookinglines[index].endDate;
+        $scope.booking.bookinglines[index].startDateAngular = new Date(moment(startDate).get('year'), moment(startDate).get('month'), moment(startDate).get('date'));
+        $scope.booking.bookinglines[index].endDateAngular = new Date(moment(endDate).get('year'), moment(endDate).get('month'), moment(endDate).get('date'));
+      }
+    }
 
      //Recherche de la catégorie séléectionné
     var loadBookings = function(){
       $scope.loading = true;
       serviceAjax.get("bookings/" + $scope.booking_id).then(function(data){
         $scope.booking = data.data;
-        console.log("donnees", $scope.booking)
-        //Autorisation pour l'utilisateur
-        if($scope.assoUser == $scope.booking.owner.id)
-          $scope.memberAssoOwner = true;
-        if($scope.assoUser == $scope.booking.booker.id)
-          $scope.memberAssoBooker = true;          
+
+        //On vérifie que l'utilisateur est admin d'une association (déjà vérifié dans l'API) 
+        // L'user peut etre admin
+        if (!$rootScope.isAdmin()) {
+          if (!$scope.booking.isAdminAsso($scope.booking.owner.login) && !$scope.booking.isAdminAsso($scope.booking.booker.login)) {
+            $location.path('/error/403')
+          }
+        }  
+        loadDates();       
       })
     }
     loadBookings();
 
+
     /* PARTIE INFORMATION GENERALE */
 
     //Remise de la caution
-    $scope.changeCaution =function(){
+    $scope.cautionReceived = function(){
       $scope.booking.cautionReceived = "Oui";
-      var booking = {};
-      booking.cautionReceived = true;
-      serviceAjax.put("bookings/" + $scope.booking.id, booking);
+      serviceAjax.post('bookings/caution/' + $scope.booking.id);
     }
 
-    //Validation de tous les items
-    $scope.acceptAll=function(){
-      serviceAjax.get("bookings/" + $scope.booking_id).then(function(data){
-        $scope.booking = data.data;
-        console.log("donnees", $scope.booking)
-        $scope.booking.statusName="Validée";
-        $scope.booking.status = "2";
-        for (var i = $scope.booking.bookinglines.length - 1; i >= 0; i--) {
-          if($scope.booking.bookinglines[i].status==1)
-            $scope.accept($scope.booking.bookinglines[i]);
-        }
+
+    //Validation de tous les items de la commande
+    $scope.acceptBooking = function(){
+      serviceAjax.get("bookings/accept/" + $scope.booking_id).then(function(){
+        serviceAjax.get("bookings/" + $scope.booking_id).then(function(res){
+          $scope.booking = res.data;
+          loadDates();
+        })
       })
     }
 
     //Annulation de la réservation
-    $scope.cancelBooking=function(){
-      for (var i = $scope.booking.bookinglines.length - 1; i >= 0; i--) {
-          $scope.cancelLine($scope.booking.bookinglines[i]);
-      }
-      $scope.booking.status = "4";
-      $scope.booking.statusName = "Annulée";
+    $scope.cancelBooking = function(){
+      serviceAjax.get('bookings/cancel/' + $scope.booking.id, $scope.booking).then(function(){
+        serviceAjax.get("bookings/" + $scope.booking_id).then(function(res){
+          $scope.booking = res.data;
+          loadDates();
+        })
+      })
     }
 
     //Validation du rendu du matériel pour tous les items
-    $scope.getBackAll=function(){
-      serviceAjax.get("bookings/" + $scope.booking_id).then(function(data){
-        $scope.booking = data.data;
-        console.log("donnees", $scope.booking)
-        $scope.booking.statusName="Terminée";
-        $scope.booking.status = "3";
-        for (var i = $scope.booking.bookinglines.length - 1; i >= 0; i--) {
-          if($scope.booking.bookinglines[i].status==2)
-            $scope.getBack($scope.booking.bookinglines[i]);
-        }
+    $scope.returnedBooking = function(){
+      serviceAjax.get('bookings/returned/' + $scope.booking.id, $scope.booking).then(function(){
+        serviceAjax.get("bookings/" + $scope.booking_id).then(function(res){
+          $scope.booking = res.data;
+          loadDates();
+        })
       })
     }
 
@@ -98,126 +101,56 @@ angular.module('bobbyApp')
     }
 
     //Accepter un item
-    $scope.accept=function($bookingline){
-      $bookingline.status = 2;
-      $bookingline.statusName = "Validé";
-      var updateBookingLine = {};
-      updateBookingLine.status = 2;
-      serviceAjax.put("bookinglines/"+$bookingline.id, updateBookingLine);
-      //Mise a jour si nécessaire du status de la commande
-      //Variable qui vérifie qu'il n'y a plus de statut "En cours"
-      var validate=0;
-      for (var i = $scope.booking.bookinglines.length - 1; i >= 0; i--) {
-        if($scope.booking.bookinglines[i].status == "1")
-          validate = -1;
-      }
-      if(validate==0){
-        $scope.booking.statusName = "Validée";
-        //Envoie de la mise a jour à la BDD
-        var updateBooking = {};
-        updateBooking.status = 2
-        serviceAjax.put("bookings/"+$scope.booking.id, updateBooking)
-      }
+    $scope.acceptLine =function(bookingline){
+      serviceAjax.get('bookinglines/accept/' + bookingline.id).then(function(res){
+        bookingline.status = 2;
+        bookingline.statusName = "Validé"
+        // On vérifie que le statut général de la commande n'a pas changé
+        serviceAjax.get('bookings/' + bookingline.booking).then(function(res){
+          if ($scope.booking.status != res.data.status) {
+            $scope.booking.status = res.data.status;
+            $scope.booking.statusName = res.data.statusName;
+          }
+        })
+      })
     }
 
     //Annuler un item
-    $scope.cancelLine=function($bookingline){
-      $bookingline.status = 4;
-      $bookingline.statusName = "Annulé";
-      var updateBookingLine = {};
-      updateBookingLine.status = 4;
-      serviceAjax.put("bookinglines/"+$bookingline.id, updateBookingLine);
-      //Mise a jour si nécessaire du status de la commande
-      //Variable qui vérifie qu'il n'y a plus de statut "En cours"
-      var validate=0;
-      //Variable qui vérifie qu'il n'y ait plus de statut "En cours et validé"
-      var rendu = 0;
-      //Variable qui vérifie qui compte le nombre de statut "Annulés"
-      var cancel=0;
-      for (var i = $scope.booking.bookinglines.length - 1; i >= 0; i--) {
-        if($scope.booking.bookinglines[i].status == "1"){
-          validate = -1;
-          rendu=-1;
-        }
-        if($scope.booking.bookinglines[i].status == "2")
-          rendu = -1;  
-        if($scope.booking.bookinglines[i].status == "4")
-          cancel++;
-      }
-      if($scope.booking.bookinglines.length == cancel){
-        $scope.booking.statusName = "Annulée";
-        //Envoie de la mise a jour à la BDD
-        var updateBooking = {};
-        updateBooking.status = 4
-        serviceAjax.put("bookings/"+$scope.booking.id, updateBooking)
-      }
-      else if(rendu==0){
-        $scope.booking.statusName = "Terminée";
-        //Envoie de la mise a jour à la BDD
-        var updateBooking = {};
-        updateBooking.status = 3
-        serviceAjax.put("bookings/"+$scope.booking.id, updateBooking)
-      }
-      else if(validate==0){
-        $scope.booking.statusName = "Validée";
-        //Envoie de la mise a jour à la BDD
-        var updateBooking = {};
-        updateBooking.status = 2
-        serviceAjax.put("bookings/"+$scope.booking.id, updateBooking)
-      }
+    $scope.cancelLine=function(bookingline){
+      serviceAjax.get('bookinglines/cancel/' + bookingline.id).then(function(res){
+        bookingline.status = 4;
+        bookingline.statusName = "Annulé"
+        // On vérifie que le statut général de la commande n'a pas changé
+        serviceAjax.get('bookings/' + bookingline.booking).then(function(res){
+          if ($scope.booking.status != res.data.status) {
+            $scope.booking.status = res.data.status;
+            $scope.booking.statusName = res.data.statusName;
+          }
+        })
+      })
     }
 
     //Item rendu
-    $scope.getBack=function($bookingline){
-      $bookingline.status = 3;
-      $bookingline.statusName = "Rendu";
-      var updateBookingLine = {};
-      updateBookingLine.status = 3;
-      serviceAjax.put("bookinglines/"+$bookingline.id, updateBookingLine);
-      //Mise a jour si nécessaire du status de la commande
-      //Variable qui vérifie qu'il n'y a plus de statut "En cours"
-      var validate=0;
-      //Variable qui vérifie qu'il n'y ait plus de statut "En cours et validé"
-      var rendu = 0;
-      for (var i = $scope.booking.bookinglines.length - 1; i >= 0; i--) {
-        if($scope.booking.bookinglines[i].status == "1"){
-          validate = -1;
-          rendu=-1;
-        }
-        if($scope.booking.bookinglines[i].status == "2")
-          rendu = -1;  
-      }
-      if(rendu==0){
-        $scope.booking.statusName = "Terminée";
-        console.log("la");
-        //Envoie de la mise a jour à la BDD
-        var updateBooking = {};
-        updateBooking.status = 3
-        serviceAjax.put("bookings/"+$scope.booking.id, updateBooking)
-      }
-      else if(validate==0){
-        $scope.booking.statusName = "Validée";
-        //Envoie de la mise a jour à la BDD
-        var updateBooking = {};
-        updateBooking.status = 2
-        serviceAjax.put("bookings/"+$scope.booking.id, updateBooking)
-      }
+    $scope.returnedLine=function(bookingline){
+      serviceAjax.get('bookinglines/returned/' + bookingline.id).then(function(res){
+        bookingline.status = 3;
+        bookingline.statusName = "Rendu"
+        // On vérifie que le statut général de la commande n'a pas changé
+        serviceAjax.get('bookings/' + bookingline.booking).then(function(res){
+          if ($scope.booking.status != res.data.status) {
+            $scope.booking.status = res.data.status;
+            $scope.booking.statusName = res.data.statusName;
+          }
+        })
+      })
     }
 
     //Mise à jour d'un item et validation
-    $scope.update=function($item){
-      var bookingline = {}
-      bookingline.status = 2;
-      $item.edit = false;
-      bookingline.quantity = $item.quantity;
-      $scope.startDate = $item.startDate;
-      $scope.endDate = $item.endDate;
-      bookingline.startDate = $filter('date')($scope.startDate, "yyyy-MM-dd");
-      bookingline.endDate = $filter('date')($scope.endDate, "yyyy-MM-dd");
-      serviceAjax.put("bookinglines/"+$item.id, bookingline).then(function(){
-        $item.status = "2";
-        $item.statusName = "Validé";
-        $item.edit = false;
+    $scope.updateItem=function(item){
+      item.startDate = $filter('date')(item.startDateAngular, "yyyy-MM-dd")
+      item.endDate = $filter('date')(item.endDateAngular, "yyyy-MM-dd")
+      serviceAjax.put("bookinglines/"+item.id, item).then(function(res){
+        item.edit = false;
       })
     }
 
