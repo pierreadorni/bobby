816d95d308e80796f7b5e09c75f076f1bf23127e
Unknown <josselin.pennors@hotmail.fr>
2019-09-19 11:37:20 +0200

    Change nav style for user profile dropdown

--- public/app/components.js
+++ public/app/components.js
@@ -580,80 +580,6 @@ angular.module('bobbyApp')
   });
 
 
-'use strict';
-
-/**
- * @ngdoc function
- * @name bobbyApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the bobbyApp
- */
-angular.module('bobbyApp')
-  .controller('categorieCtrl', function ($scope, serviceAjax, $routeParams, $location, $http, $rootScope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.categorie_id = $routeParams.id;
-
-    $scope.loading = true;
-
-    var checkLoading = function(){
-      if (!$scope.loading_types && !$scope.loading_item) {
-        $scope.loading = false;
-      }
-    }
-
-     //Recherche de la catégorie séléectionné
-    var loadCategorie = function(){
-      $scope.loading_types = true;
-      if($scope.categorie_id == 0){
-        $scope.type = 'Totalité du matériel';
-        $scope.loading_types = false;
-      }
-      else {
-        serviceAjax.get("itemtypes/"+$scope.categorie_id).then(function(data){
-          $scope.type = data.data.name;
-          $scope.loading_types = false;
-          checkLoading();
-        });
-      }
-    }
-    loadCategorie();
-
-    /* Gestion des tries des items*/
-    $scope.propertyName = 'name';
-    $scope.reverse = false;
-
-    $scope.sortBy = function(propertyName) {
-      $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
-      $scope.propertyName = propertyName;
-    };
-    
-    
-    //Chargement des items en fonction de la catégorie sélectionnée
-
-    var loadItem = function(){
-      $scope.loading_item = true;
-      serviceAjax.get("items/categories/" + $scope.categorie_id)
-        .then(function(data){
-          $scope.items = data.data;
-          $scope.loading_item = false;
-          checkLoading();
-        });
-      
-    };
-    loadItem();
-
-    // Redirection pour la demande de réservation
-    $scope.bookItem = function(item){
-      window.location.href = "#!/booking?item_id=" + item.id + "&asso_id=" + item.association_id
-    }
-
-  });
-
 
     'use strict';
 
@@ -1165,6 +1091,80 @@ app.controller('logoutCtrl', function($scope, PortailAuth) {
  * Controller of the bobbyApp
  */
 angular.module('bobbyApp')
+  .controller('categorieCtrl', function ($scope, serviceAjax, $routeParams, $location, $http, $rootScope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.categorie_id = $routeParams.id;
+
+    $scope.loading = true;
+
+    var checkLoading = function(){
+      if (!$scope.loading_types && !$scope.loading_item) {
+        $scope.loading = false;
+      }
+    }
+
+     //Recherche de la catégorie séléectionné
+    var loadCategorie = function(){
+      $scope.loading_types = true;
+      if($scope.categorie_id == 0){
+        $scope.type = 'Totalité du matériel';
+        $scope.loading_types = false;
+      }
+      else {
+        serviceAjax.get("itemtypes/"+$scope.categorie_id).then(function(data){
+          $scope.type = data.data.name;
+          $scope.loading_types = false;
+          checkLoading();
+        });
+      }
+    }
+    loadCategorie();
+
+    /* Gestion des tries des items*/
+    $scope.propertyName = 'name';
+    $scope.reverse = false;
+
+    $scope.sortBy = function(propertyName) {
+      $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+      $scope.propertyName = propertyName;
+    };
+    
+    
+    //Chargement des items en fonction de la catégorie sélectionnée
+
+    var loadItem = function(){
+      $scope.loading_item = true;
+      serviceAjax.get("items/categories/" + $scope.categorie_id)
+        .then(function(data){
+          $scope.items = data.data;
+          $scope.loading_item = false;
+          checkLoading();
+        });
+      
+    };
+    loadItem();
+
+    // Redirection pour la demande de réservation
+    $scope.bookItem = function(item){
+      window.location.href = "#!/booking?item_id=" + item.id + "&asso_id=" + item.association_id
+    }
+
+  });
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bobbyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bobbyApp
+ */
+angular.module('bobbyApp')
   .controller('placesManagementCtrl', function ($scope, serviceAjax, $location, $rootScope, $timeout, Data) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
@@ -1376,6 +1376,49 @@ angular.module('bobbyApp')
  * Controller of the bobbyApp
  */
 angular.module('bobbyApp')
+  .controller('indexItemsCtrl', function ($scope, serviceAjax, $location, $rootScope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    if(!$rootScope.isAdmin()){
+      $location.path('/error/403');
+    }
+
+    $scope.items = []
+
+
+     //Recherche de la catégorie séléectionné
+    var loadItems = function(){
+      $scope.loading = true;
+      serviceAjax.get("items").then(function(data){
+        $scope.items = data.data;
+      })
+    }
+    loadItems();
+
+    /* Tri des catégories */
+    $scope.reverse = false;
+
+    $scope.sort = function() {
+      $scope.reverse = !$scope.reverse;
+    };
+
+  });
+
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bobbyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bobbyApp
+ */
+angular.module('bobbyApp')
   .controller('categoriesManagementCtrl', function ($scope, serviceAjax, $rootScope, $timeout, Data) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
@@ -1529,49 +1572,6 @@ angular.module('bobbyApp')
       })
 
     }
-
-  });
-
-
-'use strict';
-
-/**
- * @ngdoc function
- * @name bobbyApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the bobbyApp
- */
-angular.module('bobbyApp')
-  .controller('indexItemsCtrl', function ($scope, serviceAjax, $location, $rootScope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
-    if(!$rootScope.isAdmin()){
-      $location.path('/error/403');
-    }
-
-    $scope.items = []
-
-
-     //Recherche de la catégorie séléectionné
-    var loadItems = function(){
-      $scope.loading = true;
-      serviceAjax.get("items").then(function(data){
-        $scope.items = data.data;
-      })
-    }
-    loadItems();
-
-    /* Tri des catégories */
-    $scope.reverse = false;
-
-    $scope.sort = function() {
-      $scope.reverse = !$scope.reverse;
-    };
 
   });
 
