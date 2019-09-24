5112d2ea81dd25aecd0b2524d63c183a57092f0b
Unknown <josselin.pennors@hotmail.fr>
2019-09-24 15:32:43 +0200

    Add methods in MailSender library

--- app/Libraries/MailSender.php
+++ app/Libraries/MailSender.php
@@ -16,10 +16,13 @@ class MailSender extends Model
         $bookinglines = $bookinglines;
         $comment = $comment;
         $receiver = $assoRequested["login"]."@assos.utc.fr";
+        if (env('APP_ENV') == 'local') {
+            $receiver = env('PERSONAL_ADDRESS');
+        }
 
-        Mail::send('mail',['content' => $content, 'bookinglines' => $bookinglines, 'comment' => $comment], function($message) use ($subject) {
+        Mail::send('mail',['content' => $content, 'bookinglines' => $bookinglines, 'comment' => $comment], function($message) use ($subject, $receiver) {
             $message->to('josselin.pennors@hotmail.fr');
-            $message->from('gestionfablab@gmail.com');
+            $message->from(env('MAIL_ADDRESS'));
             $message->subject($subject);
         });
     }
@@ -30,18 +33,53 @@ class MailSender extends Model
         vous recevrez un mail lorsque l'association répondra à votre demande.";
         $subject = "Bobby - Confirmation de demande de réservation de matériel";
         $receiver = $assoRequesting["login"]."@assos.utc.fr";
+        if (env('APP_ENV') == 'local') {
+            $receiver = env('PERSONAL_ADDRESS');
+        }
 
-        Mail::send('mail',['content' => $content, 'bookinglines' => $bookinglines, 'comment' => null], function($message) use ($subject) {
-            $message->to('josselin.pennors@hotmail.fr');
-            $message->from('gestionfablab@gmail.com');
+        Mail::send('mail',['content' => $content, 'bookinglines' => $bookinglines, 'comment' => null], function($message) use ($subject, $receiver) {
+            $message->to($receiver);
+            $message->from(env('MAIL_ADDRESS'));
             $message->subject($subject);
         });
     }
 
-
-    public function change_booking_status()
+    public function change_booking_status($didUpdate, $canAnnounceCaution, $isAssoReceiverRequested, $assoRequested, $assoRequesting, $bookinglines, $bookingId, $caution=null)
     {
+        $content = "La commande n°".$bookingId." a été modifiée par l'association.";
+        if ($didUpdate) {
+            $content = "Nous vous confirmons que vous avez modifié la commande n°".$bookingId.".";
+        }
 
+        $receiver = $assoRequesting["login"]."@assos.utc.fr";
+
+        if (env('APP_ENV') == 'local') {
+            $receiver = env('PERSONAL_ADDRESS');
+        }
+
+        if ($isAssoReceiverRequested) {
+            $assoRequested["login"]."@assos.utc.fr";
+        }
+        $subject = "Bobby - Demande de matériel n°".$bookingId;
+
+        Mail::send('mail_booking_edit',['content' => $content, 'bookinglines' => $bookinglines, 'canAnnounceCaution' => $canAnnounceCaution, 'caution' => $caution], function($message) use ($subject, $receiver) {
+            $message->to($receiver);
+            $message->from(env('MAIL_ADDRESS'));
+            $message->subject($subject);
+        });
+    }
+
+    public function generic_mail($subject, $content, $receiver){
+        
+        if (env('APP_ENV') == 'local') {
+            $receiver = env('PERSONAL_ADDRESS');
+        }
+
+        Mail::send('generic_mail',['content' => $content], function($message) use ($subject, $receiver) {
+            $message->to($receiver);
+            $message->from(env('MAIL_ADDRESS'));
+            $message->subject($subject);
+        });
     }
 
 }
