98be6bb7e55f0d5fda2bfe97946b76f30d01656f
Unknown <josselin.pennors@hotmail.fr>
2019-09-24 11:57:34 +0200

    Create a generic email

---
+++ resources/views/generic_mail.blade.php
@@ -0,0 +1,20 @@
+<html>
+	<head>		
+	</head>
+	<body>
+		<p>Bonjour,</p>
+        <p>{{$content}}</p>
+        <p>La réservation est disponible dans vos réservations à l'adresse suivant  : 
+            <a href="https://assos.utc.fr/bobby/#!/my_bookings">assos.utc.fr/bobby/my_bookings</a>.
+        </p>
+		<p>Ceci est un mail automatique généré depuis la plateforme <a href="https://assos.utc.fr/bobby">assos.utc.fr/bobby</a> 
+			visant à favoriser le partage de matériel entre les associations.</p>
+		<p>Pour toute question, veuillez contacter directement le Simde à l'adresse suivant:
+			<a href="mailto:simde@assos.utc.fr">simde@assos.utc.fr</a>.
+		</p>
+		<br>
+		<p>Associativement votre,</p>
+		<br>
+		<p>L'équipe du BDE et du Simde</p>
+	</body>
+</html>
