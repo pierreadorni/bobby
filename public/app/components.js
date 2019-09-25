8d4e89f9a2c23336fd8c5716435cf3cd220a51d4
Unknown <josselin.pennors@hotmail.fr>
2019-09-25 15:48:25 +0200

    Change env variable for prod in frontend

--- public/app/components.js
+++ public/app/components.js
@@ -8,6 +8,65 @@
  * Controller of the bobbyApp
  */
 angular.module('bobbyApp')
+  .controller('bugsManagementCtrl', function ($scope, serviceAjax, $location, $rootScope, $timeout, Data) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    if(!$rootScope.isAdmin()){
+      $location.path('/error/403');
+    }
+
+    $scope.error = false;
+    $scope.deleteConfirmation = false;
+
+
+    // Chargement des bugs
+    var loadBugs = function(){
+      $scope.loading = true;
+      serviceAjax.get('bugs').then(function(res){
+        $scope.bugs = res.data;
+        for (let index = 0; index < $scope.bugs.length; index++) {
+          $scope.bugs[index].loading = false;
+        }
+      })
+    }
+    loadBugs();
+
+    
+    $scope.delete = function(bug){
+      $scope.loading = true;
+      serviceAjax.delete('bugs/'+ bug.id).then(function(){
+        $scope.bugs = $scope.bugs.filter((b) => b.id != bug.id);
+        $scope.loading = false;
+        $scope.deleteConfirmation = true;
+        $timeout(function() {
+           $scope.deleteConfirmation = false;
+        }, 3000)
+      }, function(){
+        $scope.loading = false;
+        $scope.error = true;
+        $timeout(function() {
+          $scope.error = false;
+        }, 20000)
+      })
+    }
+
+  });
+
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bobbyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bobbyApp
+ */
+angular.module('bobbyApp')
   .controller('createBookingCtrl', function ($scope, serviceAjax, $routeParams, $location, Data, $filter, $rootScope, $timeout) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
@@ -544,65 +603,6 @@ angular.module('bobbyApp')
       $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
       $scope.propertyName = propertyName;
     };
-
-  });
-
-
-'use strict';
-
-/**
- * @ngdoc function
- * @name bobbyApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the bobbyApp
- */
-angular.module('bobbyApp')
-  .controller('bugsManagementCtrl', function ($scope, serviceAjax, $location, $rootScope, $timeout, Data) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
-    if(!$rootScope.isAdmin()){
-      $location.path('/error/403');
-    }
-
-    $scope.error = false;
-    $scope.deleteConfirmation = false;
-
-
-    // Chargement des bugs
-    var loadBugs = function(){
-      $scope.loading = true;
-      serviceAjax.get('bugs').then(function(res){
-        $scope.bugs = res.data;
-        for (let index = 0; index < $scope.bugs.length; index++) {
-          $scope.bugs[index].loading = false;
-        }
-      })
-    }
-    loadBugs();
-
-    
-    $scope.delete = function(bug){
-      $scope.loading = true;
-      serviceAjax.delete('bugs/'+ bug.id).then(function(){
-        $scope.bugs = $scope.bugs.filter((b) => b.id != bug.id);
-        $scope.loading = false;
-        $scope.deleteConfirmation = true;
-        $timeout(function() {
-           $scope.deleteConfirmation = false;
-        }, 3000)
-      }, function(){
-        $scope.loading = false;
-        $scope.error = true;
-        $timeout(function() {
-          $scope.error = false;
-        }, 20000)
-      })
-    }
 
   });
 
--- public/app/env.prod.js
+++ public/app/env.prod.js
@@ -6,9 +6,9 @@
   window.__env = window.__env || {};
 
   // URL de la webapp
-  window.__env.webappUrl = 'https://bobby.nastuzzi.fr';
+  window.__env.webappUrl = 'https://assos.utc.fr/bobby';
 
   // URL de l'API
-  window.__env.apiUrl = 'https://bobby.nastuzzi.fr/api/v1';
+  window.__env.apiUrl = 'https:///assos.utc.fr/bobby/api/v1';
 
 }(this));
