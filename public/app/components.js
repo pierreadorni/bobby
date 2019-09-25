c25612157798b9266021ced5ebd1b25b869de0aa
Unknown <josselin.pennors@hotmail.fr>
2019-09-25 15:54:59 +0200

    Fix issue with redirection after login

--- public/app/components.js
+++ public/app/components.js
@@ -8,65 +8,6 @@
  * Controller of the bobbyApp
  */
 angular.module('bobbyApp')
-  .controller('bugsManagementCtrl', function ($scope, serviceAjax, $location, $rootScope, $timeout, Data) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
-    if(!$rootScope.isAdmin()){
-      $location.path('/error/403');
-    }
-
-    $scope.error = false;
-    $scope.deleteConfirmation = false;
-
-
-    // Chargement des bugs
-    var loadBugs = function(){
-      $scope.loading = true;
-      serviceAjax.get('bugs').then(function(res){
-        $scope.bugs = res.data;
-        for (let index = 0; index < $scope.bugs.length; index++) {
-          $scope.bugs[index].loading = false;
-        }
-      })
-    }
-    loadBugs();
-
-    
-    $scope.delete = function(bug){
-      $scope.loading = true;
-      serviceAjax.delete('bugs/'+ bug.id).then(function(){
-        $scope.bugs = $scope.bugs.filter((b) => b.id != bug.id);
-        $scope.loading = false;
-        $scope.deleteConfirmation = true;
-        $timeout(function() {
-           $scope.deleteConfirmation = false;
-        }, 3000)
-      }, function(){
-        $scope.loading = false;
-        $scope.error = true;
-        $timeout(function() {
-          $scope.error = false;
-        }, 20000)
-      })
-    }
-
-  });
-
-
-'use strict';
-
-/**
- * @ngdoc function
- * @name bobbyApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the bobbyApp
- */
-angular.module('bobbyApp')
   .controller('createBookingCtrl', function ($scope, serviceAjax, $routeParams, $location, Data, $filter, $rootScope, $timeout) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
@@ -617,6 +558,65 @@ angular.module('bobbyApp')
  * Controller of the bobbyApp
  */
 angular.module('bobbyApp')
+  .controller('bugsManagementCtrl', function ($scope, serviceAjax, $location, $rootScope, $timeout, Data) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    if(!$rootScope.isAdmin()){
+      $location.path('/error/403');
+    }
+
+    $scope.error = false;
+    $scope.deleteConfirmation = false;
+
+
+    // Chargement des bugs
+    var loadBugs = function(){
+      $scope.loading = true;
+      serviceAjax.get('bugs').then(function(res){
+        $scope.bugs = res.data;
+        for (let index = 0; index < $scope.bugs.length; index++) {
+          $scope.bugs[index].loading = false;
+        }
+      })
+    }
+    loadBugs();
+
+    
+    $scope.delete = function(bug){
+      $scope.loading = true;
+      serviceAjax.delete('bugs/'+ bug.id).then(function(){
+        $scope.bugs = $scope.bugs.filter((b) => b.id != bug.id);
+        $scope.loading = false;
+        $scope.deleteConfirmation = true;
+        $timeout(function() {
+           $scope.deleteConfirmation = false;
+        }, 3000)
+      }, function(){
+        $scope.loading = false;
+        $scope.error = true;
+        $timeout(function() {
+          $scope.error = false;
+        }, 20000)
+      })
+    }
+
+  });
+
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bobbyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bobbyApp
+ */
+angular.module('bobbyApp')
   .controller('categorieCtrl', function ($scope, serviceAjax, $routeParams, $location, $http, $rootScope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
@@ -1149,6 +1149,15 @@ angular.module('bobbyApp')
   });
 
 
+app.controller('logoutCtrl', function($scope, PortailAuth) {
+
+
+    $scope.message = "Déconnexion";
+  
+    PortailAuth.goLogout();
+    
+});
+  
 app.controller('loginCtrl', function($scope, $location, $rootScope, $routeParams, Data, PortailAuth, serviceAjax, localStorageService, $window) {
 
 
@@ -1196,15 +1205,6 @@ app.controller('loginCtrl', function($scope, $location, $rootScope, $routeParams
 	}
 });
 
-app.controller('logoutCtrl', function($scope, PortailAuth) {
-
-
-    $scope.message = "Déconnexion";
-  
-    PortailAuth.goLogout();
-    
-});
-  
 'use strict';
 
 /**
