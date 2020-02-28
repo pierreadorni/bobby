1e9bbf6653ce6ddb31a566ef0ed00de32ac7ca4f
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2020-02-28 01:11:54 +0100

    Log replace info by warning when token issue

--- app/Http/Middleware/PortailAuth.php
+++ app/Http/Middleware/PortailAuth.php
@@ -30,12 +30,12 @@ class PortailAuth
             if (Portail::isAuthenticated($request)) {
         
                 $user = Portail::getUser();
-                \Log::info("Action effectuée par user ".$user->id." sur route ".$request->method()."::".$request->path());
+                \Log::warning("Action effectuée par user ".$user->id." sur route ".$request->method()."::".$request->path());
                 return $next($request);
             
             } else {
 
-                \Log::info("401, unauthorized, token not recognized");
+                \Log::error("401, unauthorized, token not recognized");
                 return response()->json(array("error" => "401, unauthorized, token not recognized"), 401);
             
             }
@@ -43,49 +43,21 @@ class PortailAuth
         } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
 
             // Le token est expiré
-            \Log::info("401, unauthorized, token expired");
+            \Log::warning("401, unauthorized, token expired");
             return response()->json(array("error" => "401, unauthorized, token expired"), 401);
 
         } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
 
             // Le token n'est pas reconnu
-              \Log::info("401, unauthorized, token not recognized");
+              \Log::warning("401, unauthorized, token not recognized");
               return response()->json(array("error" => "401, unauthorized, token not recognized"), 401);
 
         } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
 
             // Absence du header Authorization
-            \Log::info("401, unauthorized, token not provided");
+            \Log::warning("401, unauthorized, token not provided");
             return response()->json(array("error" => "401, unauthorized, token not provided"), 401);
         
         }
-
-        // if($request->header("Authorization")) {
-
-            // Récupère du token
-            // $token = $request->header("Authorization");
-
-            // Authenticate the user based on the token
-            // Portail::authenticateUser($request);
-            // $payload = JWTAuth::parseToken()->getPayload();
-
-            // Portail::authenticateUser($payload);
-
-            // if (Portail::isAuthenticated($request)) {
-            
-            //     return $next($request);
-            
-            // } else {
-             
-            //     return response()->json(array("error" => "401, unauthorized, token not recognized"), 401);
-            
-            // }
-        
-        // } else {
-       
-        //     // Absence du header Authorization
-        //     return response()->json(array("error" => "401, unauthorized, token not provided"), 401);
-
-        // }
     }
 }
